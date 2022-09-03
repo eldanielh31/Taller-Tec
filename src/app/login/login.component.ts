3610aa1f6ffa2007292708ca353c548145ce114e
Daniel <danbg31@gmail.com>
2022-09-03 17:30:04 -0600

    login start

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm:FormGroup;
 
-  constructor() { }
+  username: string = ''
+  password: string = ''
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+  handleLogin() {
+    this.username = this.loginForm.get('username')?.value;
+    console.log(this.username);
+    this.password = this.loginForm.get('password')?.value;
+    console.log(this.password);
+  }
 
   ngOnInit(): void {
   }
+
+  
+
 
 }
