fe31883cf9db749e9f56f6dca154b837f50dbe7a
Daniel <danbg31@gmail.com>
2022-09-10 21:50:14 -0600

    añadido primer ejemplo localstorage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,23 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  constructor(private localStorage: StorageService, private router: Router) {}
+
+  email: string = ''
+  password: string = ''
 
   ngOnInit() {
   }
   ngOnDestroy() {
   }
+
+  handleLogin(){
+    this.localStorage.saveData('email', this.email)
+    this.localStorage.saveData('password', this.password)
+
+    this.router.navigate(['/dashboard'])
+  }
+
 
 }
