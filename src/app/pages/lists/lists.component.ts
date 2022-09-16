ff1f99978780e7578f242a5fa8f1b0ac92bddd33
Roy Chavarría <roychav99@gmail.com>
2022-09-16 16:17:45 -0600

    ngfor

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import workerData from 'API/Database/Tables/employees.json';
+
+interface Worker{
+  idNumber: Number;
+  identification: Number;
+  name: String,
+  lastName: String;
+  cellphoneNumber: Number;
+  joiningDate: String;
+  birthDate: String;
+  age: Number;
+  role: String;
+  branch: String;
+  email: String;
+  address: String;
+  username: String;
+  password: String;
+}
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
-export class ListsComponent implements OnInit {
 
-  constructor(private router: Router) { }
+
+
+export class ListsComponent implements OnInit {
+  data: Array<any>;
+  constructor() { 
+    this.data = [
+      { firstName: 'John', lastName: 'Doe', age: '35' },
+      { firstName: 'Michael', lastName: 'Smith', age: '39' },
+      { firstName: 'Michael', lastName: 'Jordan', age: '45' },
+      { firstName: 'Tanya', lastName: 'Blake', age: '47' }
+  ];
+    
+  }
 
   ngOnInit(): void {
   }
 
+  
+  workers: Worker[] = workerData; 
   handleclick(){
     
   }
 
+  
 }
