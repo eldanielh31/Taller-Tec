c7e73e4713873f12ed55e70d6294733ea272e41c
Daniel Zúñiga <danyazunigab@gmail.com>
2022-09-15 23:24:58 -0600

    Updated Appointment Model

--- API/Models/Appointment.cs
+++ API/Models/Appointment.cs
@@ -5,8 +5,11 @@ public class Appointment
     public int idAppointment{get;set;}
     public string idService{get;set;}
     public int idClient{get;set;}
+    public string clientName{get;set;}
+    public string clientLastName{get;set;}
     public int idEmployee{get;set;}
-    public int price{get;set;}
+    public string employeeName{get;set;}
+    public string employeeLastName{get;set;}
 
     public void setId(int newId)
     {
