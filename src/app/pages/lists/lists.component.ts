84f51e4de73b1f9c56cc54f1350dfd9f2d7a10ac
Roy Chavarría <roychav99@gmail.com>
2022-09-15 23:07:05 -0600

    Creada pagina lista

---
+++ src/app/pages/lists/lists.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-lists',
+  templateUrl: './lists.component.html',
+  styleUrls: ['./lists.component.css']
+})
+export class ListsComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  handleclick(){
+    
+  }
+
+}
